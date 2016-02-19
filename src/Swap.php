3ef7c2ac9a89e94141fb49e83a5a65c883182119
Marika Allely <mballely@gmail.com>
2016-02-18 17:10:52 -0800

    add function for ignoring partial matches

--- src/Swap.php
+++ src/Swap.php
@@ -2,11 +2,26 @@
 class Swap
 {
 
-    function wordSwap($input_word, $replacement_word, $input_phrase)
+    function wordSwapCaseMatch($input_phrase, $input_word, $replacement_word)
+    {
+
+        $split_phrase = explode(" ", $input_phrase);
+        foreach ($split_phrase as $index => $word) {
+            if ($split_phrase[$index] == $input_word) {
+                $split_phrase[$index] = str_replace($input_word, $replacement_word, $word);
+            }
+        }
+        $input_phrase = implode(" ", $split_phrase);
+        return $input_phrase;
+    }
+
+    function wordSwapAnyMatch($input_word, $replacement_word, $input_phrase)
     {
         $output = str_replace($input_word, $replacement_word, $input_phrase);
         return $output;
     }
 }
+
+// preg_replace('/[^a-z]/i', '', $string)
 
  ?>
--- tests/SwapTest.php
+++ tests/SwapTest.php
@@ -12,7 +12,7 @@
             $input_phrase = "Hello young padowan";
 
             // Act
-            $result = $test_Swap->wordSwap($input_word, $replacement_word, $input_phrase);
+            $result = $test_Swap->wordSwapAnyMatch($input_word, $replacement_word, $input_phrase);
             // Assert
             $this->assertEquals("Goodbye young padowan", $result);
         }
@@ -26,9 +26,23 @@
             $input_phrase = "That cat is cute, but my cat is better.";
 
             // Act
-            $result = $test_Swap->wordSwap($input_word, $replacement_word, $input_phrase);
+            $result = $test_Swap->wordSwapAnyMatch($input_word, $replacement_word, $input_phrase);
             // Assert
             $this->assertEquals("That dog is cute, but my dog is better.", $result);
+        }
+
+        function test_wordSwap_partial_match()
+        {
+            // Arrange
+            $test_Swap = new Swap;
+            $input_word = "cat";
+            $replacement_word = "dog";
+            $input_phrase = "Let's play catch with the cat .";
+
+            // Act
+            $result = $test_Swap->wordSwapCaseMatch($input_phrase, $input_word, $replacement_word);
+            // Assert
+            $this->assertEquals("Let's play catch with the dog .", $result);
         }
     }
  ?>
